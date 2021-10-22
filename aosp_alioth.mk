010e0793ea5a12eb63b1e090f35ebbca076c6eca
manish4586 <manish.n.manish45@gmail.com>
2021-10-23 00:15:47 +0530

    alioth: update overlay and official
    Signed-off-by: manish4586 <manish.n.manish45@gmail.com>

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -7,12 +7,26 @@
 
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit from  HavocOS.
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, vendor/aosp/config/common.mk)
 
-# Basic Target Flags 
+# Gapps
+USE_GAPPS := true
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 
+# Maintainer
+ROHIE_MAINTAINER := Manish4586
+ROHIE_BUILD_TYPE := OFFICIAL
+
+# Density
+TARGET_SCREEN_DENSITY := 480
+
+# Basic Target Flags 
 PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := xiaomi
@@ -21,7 +35,4 @@ PRODUCT_MODEL := Mi 11X
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Extra Flags
-
-TARGET_SCREEN_DENSITY := 450
-TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
--- device.mk
+++ device.mk
@@ -7,7 +7,6 @@
 BOARD_BOOT_HEADER_VERSION := 3
 
 # Inherit from sm8250-common
-
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)
 
@@ -15,11 +14,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Audio
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/xiaomi/alioth/audio,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Device-specific settings
-
 PRODUCT_PACKAGES += \
     XiaomiDoze \
     XiaomiParts \
@@ -31,19 +28,17 @@ PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
 
 # Keylayout
-
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
      $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
      $(LOCAL_PATH)/keylayout/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc
 
 # Overlays
-
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay 
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp
 
 # Properties
-
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
